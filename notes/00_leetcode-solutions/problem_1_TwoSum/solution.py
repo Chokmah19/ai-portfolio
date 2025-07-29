79011673926fd824556bea086a938590b61ecb87
Debby <53501393+Chokmah19@users.noreply.github.com>
2025-07-29 17:07:49 +0800

    Update solution.py
    add solution

--- notes/00_leetcode-solutions/problem_1_TwoSum/solution.py
+++ notes/00_leetcode-solutions/problem_1_TwoSum/solution.py
@@ -1,1 +1,12 @@
-add folder for leetcode problem 1 two sum 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #hash table
+        #建立一個空的字典
+        seen = {} 
+        #邊走邊檢查,要記得讓i跑到最後一個元素
+        for i in range(len(nums)): 
+            x = nums[i]
+            complement = target - x
+            if complement in seen:
+                return [seen[complement], i]
+            seen[x] = i
